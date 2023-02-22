b0e67e1f71e72e3f44af4636ae5074eb072acd72
William Rogers <williamrogersdev@gmail.com>
2023-02-21 19:14:00 -0600

    initial commit

---
+++ data.h
@@ -0,0 +1,20 @@
+
+/*
+ * A header file for a simple ADT that holds an int and a string
+ */
+
+#ifndef DEMO_DATA_H
+#define DEMO_DATA_H
+
+#include <string>
+using std::string;
+
+/*
+ * Simple data structure to simulate the idea of an ADT which contains
+ */
+struct Data {
+    int id;
+    string data;
+};
+
+#endif /* DEMO_DATA_H */
